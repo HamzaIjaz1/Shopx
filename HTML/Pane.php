8eb3ab61e636250cfb83ba59f8b5acd23eb1d33b
HamzaIjaz1 <iamhamzza@gmail.com>
2018-11-18 12:49:03 +0500

    Shopx Repository.

--- HTML/Pane.php
+++ HTML/Pane.php
@@ -6,6 +6,8 @@
     <link rel="stylesheet" href="../bootstrap-4.1.3/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="../CSS/PostStyle.css">
     <link rel="stylesheet" href="../fontawesome-free-5.4.2-web/css/all.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css">
+
 
 </head>
 <body>
---
+++ HTML/Reviews.php
@@ -0,0 +1,34 @@
+<?php include 'Pane.php';?>
+<head>
+    <link rel="stylesheet" href="../CSS/Reviews%20Style.css">
+</head>
+
+<div class="container">
+    <h2 class="text-center">Bootstrap 4 User Rating Form / Comment Form</h2>
+
+    <div class="card">
+        <div class="card-body">
+            <div class="row">
+                <div class="col-md-2">
+                    <img src="../Images/placeholder-man.png" class="img img-rounded img-fluid"/>
+                    <p class="text-secondary text-center">15 Minutes Ago</p>
+                </div>
+                <div class="col-md-10">
+                    <p>
+                        <a class="float-left" href="#"><strong>Username</strong></a>
+                        <span class="float-right"><i class="text-warning fa fa-star"></i></span>
+                        <span class="float-right"><i class="text-warning fa fa-star"></i></span>
+                        <span class="float-right"><i class="text-warning fa fa-star"></i></span>
+                        <span class="float-right"><i class="text-warning fa fa-star"></i></span>
+                    </p>
+                    <div class="clearfix"></div>
+                    <p>Lorem Ipsum is simply dummy text of the pr make  but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                    <img src="../Images/image-placeholder.png" alt="Product" class="img img-fluid">
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
